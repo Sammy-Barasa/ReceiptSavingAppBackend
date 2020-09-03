cb3d7ef7649a0985feae810c4aeb2cffe59a5183
Sammy-Barasa <kesammybarasa@gmail.com>
2020-09-03 13:03:17 +0300

    updated production settings

--- ReceiptSaverBackend/settings.py
+++ ReceiptSaverBackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '!_rks3+g)kz$(j01ml7^y_+uukqww8n3^^09vyj9=an%ly#st9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = [
